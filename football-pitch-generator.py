f24fffda3fa9a080320d65fc62f36279d13c6a27
manuelarte <manueldoncelmartos@gmail.com>
2025-07-26 20:53:57 +0200

    adding scripts

---
+++ football-pitch-generator.py
@@ -0,0 +1,36 @@
+import re
+
+
+class FootballPitchVars:
+    pitch_length: int
+    pitch_width: int
+    pitch_grass_cut_width: int
+    extra_space: int
+    def __init__(self):
+        self.pitch_length = 120
+        self.pitch_width = 90
+        self.pitch_grass_cut_width = 10
+        self.extra_space = 3
+
+def main():
+    with open("football-pitch.svg.template") as f:
+        print("Template file read")
+        template: str = f.read()
+        template_vars: FootballPitchVars = FootballPitchVars()
+        for pitch in template_vars.__dict__:
+            to_replace: str = f"{{{{ ${pitch} }}}}"
+            to = getattr(template_vars, pitch)
+            print(f"Replacing {to_replace}: {to}")
+            template = template.replace(to_replace, str(to))
+
+        eval_re = re.compile(r"{{ eval\((.*?)\) }}")
+        for s in re.findall(eval_re, template):
+            evaled = eval(s)
+            print(f"Evaluating {s} = {evaled}")
+            template = template.replace(f"{{{{ eval({s}) }}}}", str(evaled))
+        with open(f"football-pitch-parsed-{template_vars.pitch_length}_{template_vars.pitch_width}.svg", "w") as text_file:
+            text_file.write(template)
+            print("SVG file created")
+
+if __name__ == "__main__":
+    main()
