72a8721871e8e146829bce7bc3e5b567dbd939ca
manuelarte <manueldoncelmartos@gmail.com>
2025-07-26 19:09:55 +0200

    adding more things

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,13 @@
 <template>
+ <v-toolbar>
+  <v-toolbar-title>Football Pitch SVG Generator</v-toolbar-title>
+   <v-btn icon="mdi-github" @click="goToGitHubPage()"></v-btn>
+ </v-toolbar>
   <HelloWorld />
 </template>
 
 <script lang="ts" setup>
-  //
+  const goToGitHubPage = (): void => {
+    window.open("https://github.com/manuelarte/footballpitchsvggenerator")
+  }
 </script>
